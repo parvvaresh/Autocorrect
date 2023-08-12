075e38a3a5b07118b5f262464801dd0a5cb5b610
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-08-12 23:53:57 +0330

    update farsi language

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ def main():
 	lng_input = input("please enter a langauge  (en / fa) : ")
 
 	while lng_input.lower() not in ["en", "fa"]:
-		lng_input = input("please enter a langauge : (en / fa)")
+		lng_input = input("please enter a langauge : (en / fa) : ")
 
 	text = input("please enter words : ")
 	text = pr.process(text, lng_input)
+	print(text)
 
 	result = gw.get(text, lng_input)
 
-	if result == 1:
-		print("correct")
+	
 
-	else:
-		print("		edit distance model		")
-		for index, word in enumerate(result["edit distance"]):
-			print(f"		 {index + 1}----> {word}")
 
-		print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+	print("		edit distance model		")
+	for index, word in enumerate(result["edit distance"]):
+		print(f"		 {index + 1}----> {word}")
 
-		print("		jaccard similarity model		")
-		for index, word in enumerate(result["jaccard similarity"]):
-			print(f"		 {index + 1}----> {word}")
+	print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+	print("		jaccard similarity model		")
+	for index, word in enumerate(result["jaccard similarity"]):
+		print(f"		 {index + 1}----> {word}")
 
 main()
+
